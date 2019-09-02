32ead5086014ac81da390825f6eac5eea57516af
amr omar <a.ahmed@hyvetechnology.com>
2019-09-02 12:58:30 +0200

    upload dev config

---
+++ backend/config/dev.js
@@ -0,0 +1,5 @@
+// dev.js - development keys here!!
+module.exports = {
+	mongoURI: 'mongodb+srv://Amr_Dev:dQjfrf8O0A4uMwDX@private-cuuee.mongodb.net/dashboard',
+	jwtPrivateKey: 'MySectretKey',
+};
